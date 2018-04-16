0be057e6ae4c7f07363a61dfc6aa7b0501f769a3
Su Yang <ssyang@mit.edu>
2018-04-16 01:33:36 -0400

    Add commenting to new posts

--- js/index.js
+++ js/index.js
@@ -23,6 +23,15 @@ $("#submit-post").click(function(e) {
 	var title = $("#post-title").val();
 	var content = $("#post-content").val();
 	$("#feed").prepend('<div class="post" id="3"><div class="top-container"><div class="info-container"><div class="profile-container"><img src="../img/profile-pictures/jane-doe.png"><p>Su Yang</p></div><div class="like-container"><img src="../img/heart-white.svg" onclick="likePost(3, true, 0)"><p>Like</p><p class="number-likes">0 likes</p></div></div><div class="content-container"><h1>' + title + '</h1><p>' + content + '</p></div></div><div class="bottom-container"><div class="comment-container"></div><input class="add-comment" type="text" placeholder="Add a comment..." id="post-3"></div></div>');
+	$(".add-comment").keypress(function(e) {
+		var postID = $(this).attr("id").split("-")[1];
+		if (e.which == 13) {
+			var container = $("#" + postID + " .bottom-container .comment-container");
+			var content = $("#" + postID + " .add-comment").val();
+			container.prepend("<p class='comment'><span class='commenter'>Lucy</span>" + content + "<span class='time-ago'>Just now</span></p>");
+			$(".add-comment").val("");
+		};
+	});
 });
 
 $("#searchbox").keypress(function(e) {
@@ -41,6 +50,7 @@ $("#add-post")
 });
 
 $(".add-comment").keypress(function(e) {
+	console.log('here');
 	var postID = $(this).attr("id").split("-")[1];
 	if (e.which == 13) {
 		var container = $("#" + postID + " .bottom-container .comment-container");
