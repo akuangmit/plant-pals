fec9456fdcff56df2d25a5baf3a7a0f6e054dd1d
Su Yang <ssyang@mit.edu>
2018-04-15 22:02:46 -0400

    Add search functionality

--- js/index.js
+++ js/index.js
@@ -5,3 +5,10 @@ function likePost(id, like, currentLikes) {
 		$("#" + id + " .like-container").html("<img src='../img/heart-white.svg' onclick='likePost(" + id + ", true, " + (currentLikes - 1) + ")'><p>Like</p><p class='number-likes'>" + (currentLikes - 1) + " likes</p>");
 	};
 };
+
+$("#searchbox").keypress(function(e) {
+	if (e.which == 13) {
+		window.location.replace("search.html");
+		return false;
+	};
+});
