c39d4ee539716dfffeb8a5a610046f92fa8a9b64
Su Yang <ssyang@mit.edu>
2018-04-19 22:29:58 -0400

    Make search dynamic

--- js/global.js
+++ js/global.js
@@ -1,6 +1,12 @@
 $("#searchbox").keypress(function(e) {
 	if (e.which == 13) {
+		var term = $("#searchbox").val();
+		localStorage.setItem('searchTerm', term);
 		window.location.replace("search.html");
 		return false;
 	};
+});
+
+$(document).ready(function() {
+    $(".query").text(localStorage.getItem('searchTerm'));
 });
