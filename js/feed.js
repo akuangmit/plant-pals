3fbf4730cdd478b1b51a35339d788563364fc4cc
Su Yang <ssyang@mit.edu>
2018-05-02 01:12:19 -0400

    Change post display

--- js/feed.js
+++ js/feed.js
@@ -11,11 +11,12 @@ function add_comment(divID, comment, animate) {
 	$(".add-comment").val("");
 	var container = $("#" + divID + " .bottom-container .comment-container");
 	if (animate) {
-		var newComment = $("<p class='comment'><span class='commenter'>"+ author + "</span>" + content + "<span class='time-ago'>"+time+"</span></p>").hide();
+		var newComment = $("<p class='comment'><span class='commenter'>"+ author + "</span>" + content + "</p>").hide();
+		//<span class='time-ago'>"+time+"</span>
 		container.append(newComment);
 		newComment.show('slow');
 	} else {
-		var newComment = $("<p class='comment'><span class='commenter'>"+ author + "</span>" + content + "<span class='time-ago'>"+time+"</span></p>");
+		var newComment = $("<p class='comment'><span class='commenter'>"+ author + "</span>" + content + "</p>");
 		container.append(newComment);
 	}
 }
@@ -61,7 +62,25 @@ function add_post(divID, globalID, post, container) {
 		showAllMessage = "View All "+comments.length + " Comments";
 	}
 	// var showAllMessage = "View All "+comments.length + " Comments";
-	container.prepend('<div class="post" id="'+divID+'"><div class="top-container"><div class="info-container"><div class="profile-container"><img src="../img/profile-pictures/'+pfpfilename+'"><a href="profile.html" class="author" onclick="set_profile(' + globalID + ')">' + author + '</a></div><div class="like-container">' + like_img_div + '</div></div><div class="content-container"><h1>' + title + '</h1><p>' + image_div + '<div class="content-text">' + content + '</div></p></div></div><div class="bottom-container"><div class="show-all-comments'+divID+'" id="show-comments">'+showAllMessage+'</div><div class="comment-container"></div><input class="add-comment" type="text" placeholder="Add a comment..." id="post-'+divID+'"></div></div>');
+	//container.prepend('<div class="post" id="'+divID+'"><div class="top-container"><div class="info-container"><div class="profile-container"><img src="../img/profile-pictures/'+pfpfilename+'"><a href="profile.html" class="author" onclick="set_profile(' + globalID + ')">' + author + '</a></div><div class="like-container">' + like_img_div + '</div></div><div class="content-container"><h1>' + title + '</h1><p>' + image_div + '<div class="content-text">' + content + '</div></p></div></div><div class="bottom-container"><div class="show-all-comments'+divID+'" id="show-comments">'+showAllMessage+'</div><div class="comment-container"></div><input class="add-comment" type="text" placeholder="Add a comment..." id="post-'+divID+'"></div></div>');
+	container.prepend('<div class="post" id="' + divID + '"><div class="profile-container"><img src="../img/profile-pictures/'+pfpfilename+'"><a href="profile.html" class="author" onclick="set_profile(' + globalID + ')">' + author + '</a></div><div class="content-container">' + image_div + '<span class="title">' + title + '</span><div class="content-text">' + content + '</div></div><div class="bottom-container"><div class="show-all-comments'+divID+'" id="show-comments">'+showAllMessage+'</div><div class="comment-container"></div><input class="add-comment" type="text" placeholder="Add a comment..." id="post-'+divID+'"></div></div></div>');
+	// <div class="profile-container">
+	// 	<img src="../img/profile-pictures/'+pfpfilename+'">
+	// 	<a href="profile.html" class="author" onclick="set_profile(' + globalID + ')">' + author + '</a>
+	// </div>
+	// <div class="content-container">
+	// 	<h1>' + title + '</h1><p>' + image_div + '<div class="content-text">' + content + '</div></p>
+	// </div>
+	//<div class="like-container">' + like_img_div + '</div>
+	// <div class="bottom-container">
+	// 	<div class="show-all-comments'+divID+'" id="show-comments">'
+	// 		+showAllMessage+
+	// 	'</div>
+	// 	<div class="comment-container">
+	// 	</div>
+	// 	<input class="add-comment" type="text" placeholder="Add a comment..." id="post-'+divID+'">
+	// 	</div>
+	// </div>
 
 	// if there are too many comments, make the show all comments button visible
 	if (comments.length > max_comments) {
