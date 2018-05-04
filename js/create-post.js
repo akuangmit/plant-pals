23763f8905911bbef9edc4b327cdea71e57c9b4b
Su Yang <ssyang@mit.edu>
2018-05-03 20:36:51 -0400

    Update create post modal

--- js/create-post.js
+++ js/create-post.js
@@ -1,21 +1,31 @@
 $("textarea").click(function() {
-	$("textarea").animate({"height":"150px"}, 100);
+	$("textarea").animate({"height":"150px", "margin-top": "20px"}, 100);
 	$(".create-post button").show(100);
-	$(".create-post #file-upload").show(100);
+	$(".create-post label").show(100);
 	$(".create-post p").show(100);
-	$(".create-post input").show(100);
-	$(".up").show(100);
-	$(".create-post").animate({"padding-bottom":"50px"}, 100);
+	$(".create-post #post-title").show(100);
+	$(".create-post .close").show(100);
+	$(".create-post #upload-hint-container").show(100);
+	$(".create-post").animate({"padding-bottom":"5px"}, 100);
+	$("#create-post-bottom").animate({"padding":"10px 0"}, 100);
 });
 
-$(".up").click(hide_create_post)
+// $(".create-post .close").click(hide_create_post)
+$("body").click(function(e) {
+	console.log(e.target.id);
+	if (e.target.id != "create-post" && e.target.id != "create-post-container" && e.target.id != "file-icon" && e.target.id != "photo-upload-container" && e.target.id != "upload-hint-container" && e.target.id != "submit-post-div" && e.target.id != "submit-post" && e.target.id != "create-post-bottom" && e.target.id != "upload-hint" && e.target.id != "file-upload" && e.target.id != "post-content" && e.target.id != "post-title") {
+		hide_create_post();
+	};
+})
 
 function hide_create_post() {
-	$("textarea").animate({"height":"100px"}, 100);
+	$("textarea").animate({"height":"60px", "margin-top": "0px"}, 100);
 	$(".create-post button").hide(100);
-	$(".create-post #file-upload").hide(100);
+	$(".create-post label").hide(100);
 	$(".create-post p").hide(100);
-	$(".create-post input").hide(100);
-	$(".up").hide(100);
+	$(".create-post #post-title").hide(100);
+	$(".create-post .close").hide(100);
+	$(".create-post #upload-hint-container").hide(100);
 	$(".create-post").animate({"padding-bottom":"20px"}, 100);
+	$("#create-post-bottom").animate({"padding":"0"}, 100);
 }
